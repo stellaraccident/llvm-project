6a5bebb7b0b7df9e96fe64c9db1c762036646a75
Stella Laurenzo <stellaraccident@gmail.com>
2021-08-20 13:30:56 -0700

    [mlir][python] Simplify python extension loading.
    * Now that packaging has stabilized, removes old mechanisms for loading extensions, preferring direct importing.
    * Removes _cext_loader.py, _dlloader.py as unnecessary.
    * Fixes the path where the CAPI dll is written on Windows. This enables that path of least resistance loading behavior to work with no further drama (see: https://bugs.python.org/issue36085).
    * With this patch, `ninja check-mlir` on Windows with Python bindings works for me, modulo some failures that are actually due to a couple of pre-existing Windows bugs. I think this is the first time the Windows Python bindings have worked upstream.
    * Downstream changes needed:
      * If downstreams are using the now removed `load_extension`, `reexport_cext`, etc, then those should be replaced with normal import statements as done in this patch.
      * If non standard build setups are replacing `mlir/_mlir_libs/__init__.py` with their own copy, that copy should include something that does the equivalent of below:
    
    ```
    def _configure_native_extension():
      import logging
      from . import _mlir
      mlir_libs_suffix = "._mlir_libs"
      if __package__.endswith(mlir_libs_suffix):
        # Form a new relative name to the .dialects package, which should be
        # a peer of this _mlir_libs package.
        dialects_package = __package__[0:-len(mlir_libs_suffix)] + ".dialects"
        logging.debug("MLIR dialects search package: %s", dialects_package)
        _mlir.globals.append_dialect_search_prefix(dialects_package)
      else:
        logging.warning("Expected package to be of form *._mlir_libs but got %s",
                        __package__)
    
    _configure_native_extension()
    ```
    
    The key thing is that `_mlir.globals.append_dialect_search_prefix` is now done here instead of in `_cext_loader` (which no longer exists).

--- mlir/python/mlir/_mlir_libs/__init__.py
+++ mlir/python/mlir/_mlir_libs/__init__.py
@@ -4,23 +4,9 @@
 
 from typing import Sequence
 
-import importlib
 import os
 
-__all__ = [
-  "load_extension",
-  "preload_dependency",
-]
-
 _this_dir = os.path.dirname(__file__)
-
-def load_extension(name):
-  return importlib.import_module(f".{name}", __package__)
-
-
-def preload_dependency(public_name):
-  # TODO: Implement this hook to pre-load DLLs with ctypes on Windows.
-  pass
 
 
 def get_lib_dirs() -> Sequence[str]:
@@ -39,3 +25,23 @@ def get_include_dirs() -> Sequence[str]:
   not be present.
   """
   return [os.path.join(_this_dir, "include")]
+
+
+# Eagerly load the main _mlir extension and configure it with our actual
+# package location.
+def _configure_native_extension():
+  import logging
+  from . import _mlir
+  mlir_libs_suffix = "._mlir_libs"
+  if __package__.endswith(mlir_libs_suffix):
+    # Form a new relative name to the .dialects package, which should be
+    # a peer of this _mlir_libs package.
+    dialects_package = __package__[0:-len(mlir_libs_suffix)] + ".dialects"
+    logging.debug("MLIR dialects search package: %s", dialects_package)
+    _mlir.globals.append_dialect_search_prefix(dialects_package)
+  else:
+    logging.warning("Expected package to be of form *._mlir_libs but got %s",
+                    __package__)
+
+
+_configure_native_extension()
